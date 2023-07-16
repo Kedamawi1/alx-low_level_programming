b37c775289be41891edde58c8e6c135b9fc14e3d
Abenezer azariya <135222633+Kedamawi1@users.noreply.github.com>
2023-07-16 10:49:09 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - Entry point
  * Return: ALways 0 (Success)
  */
+
 int main(void)
 {
         int n, m;
