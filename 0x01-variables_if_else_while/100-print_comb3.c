81cd41cddf19c2639c4f2eaf88c0c7199f650304
Abenezer azariya <135222633+Kedamawi1@users.noreply.github.com>
2023-07-16 11:06:19 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,33 +11,10 @@ int main(void)
 
 {
         
-        int n, m;
+        int d, p;
         
-        for (n = 48; n <= 56; n++)
+        for (d = '0'; d < '9'; d++)
         {
                 
-                for (m = 49; m <= 57; m++)
-                {
-                        
-                        if (m > n)
-                        {
-                                
-                                putchar(n);
-                                putchar(m);
-                                if (n != 56 || m != 57)
-                                {
-                                        
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                                
-                        }
-                        
-                }
-                
-        }
-        
-        putchar('\n');
-        return (0);
-        
-}
+                for (p = d + 1; p <= '9'; p++)
+                {   
