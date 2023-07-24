1412f722cd1b90acc08f1cc0ae76ba8d20e4f708
Abenezer azariya <135222633+Kedamawi1@users.noreply.github.com>
2023-07-24 13:53:58 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 - a function Takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input
- * Return: n
+ * @n:input
+ * Return:n
  */
-voidreset_to_98(int *n)
+void reset_to_98(int *n)
 {        
        *n = 98;       
 }
